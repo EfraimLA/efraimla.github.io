ff912b8d5559163c9da811189c8d1d021c33c0c0
Efraim LA <efraim.lopez@fonyou.com>
2023-06-26 16:39:27 -0600

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang='ts'>
 	import { _ } from 'svelte-i18n';
+	import { onMount } from 'svelte';
+
+	let showScrollArrow = true;
+
+	onMount(async () => window.addEventListener('scroll', () => showScrollArrow = window.scrollY <= 400));
 </script>
 
 <div id='home'>
@@ -31,12 +36,60 @@
 	</div>
 
 	<!-- Scroll Arrow -->
-	<div id='scroll_arrow' class='absolute flex w-screen justify-center bottom-2'>
-		<svg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6 arrow' fill='none' viewBox='0 0 24 24' stroke='currentColor'
-				 stroke-width='2'>
-			<path stroke-linecap='round' stroke-linejoin='round' d='M16 17l-4 4m0 0l-4-4m4 4V3' />
-		</svg>
+	{#if showScrollArrow}
+		<div id='scroll_arrow' class='absolute flex w-screen justify-center bottom-2'>
+			<svg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6 arrow' fill='none' viewBox='0 0 24 24'
+					 stroke='currentColor'
+					 stroke-width='2'>
+				<path stroke-linecap='round' stroke-linejoin='round' d='M16 17l-4 4m0 0l-4-4m4 4V3' />
+			</svg>
+		</div>
+	{/if}
+
+	<!-- My Stack Section -->
+	<div id='stack' class='text-center font-mono font-medium'>
+
+		<h1 class='font-medium'>My Stack</h1>
+		<p class='text-gray-600 dark:text-gray-300 text-sm font-light tracking-tight leading-tight md:text-base'>These are
+			some languages and technologies i've used before.
+		</p>
+
+		<h2 class='text-xl font-medium'>Languages</h2>
+		<div class='w-full flex justify-evenly flex-wrap items-center icons'>
+			<img src='/svg/java_logo.svg' class='w-8' alt='Java Logo'>
+			<img src='/svg/python_logo.svg' class='w-8' alt='Python Logo'>
+			<img src='/svg/typescript_logo.svg' class='w-8' alt='Typescript Logo'>
+		</div>
+
+		<h2 class='text-xl font-medium'>Frameworks</h2>
+		<div class='w-full flex justify-evenly flex-wrap items-center mt-2 icons'>
+			<img src='/svg/angular_logo.svg' class='w-12' alt='Angular Logo'>
+			<img src='/svg/vuejs_logo.svg' class='w-10' alt='Vue Logo'>
+			<img src='/svg/quarkus_logo.svg' class='w-8' alt='Quarkus Logo'>
+			<img src='/svg/vertx_logo.svg' class='w-20' alt='Vertx Logo'>
+		</div>
+
+		<h2 class='text-xl font-medium'>Databases</h2>
+		<div class='w-full flex justify-evenly flex-wrap items-center mt-2 icons'>
+			<img src='/svg/postgresql_logo.svg' class='w-10' alt='PostgreSQL Logo'>
+			<img src='/svg/mongodb_logo.svg' class='w-36' alt='MongoDB Logo'>
+		</div>
 	</div>
+
+	<!-- My Projects Section -->
+	<!-- <div id="projects" class="text-center font-mono font-medium">
+		<h1 class="font-medium">My Projects</h1>
+		<p class="text-gray-600 dark:text-gray-300 text-sm font-light tracking-tight leading-tight md:text-base">Here are
+			some
+			projects i've worked on.
+		</p>
+		<div class="w-full flex justify-evenly flex-wrap items-center mt-2">
+			<div class="w-full flex flex-col items-center justify-center">
+				<img src="assets/svg/logo_github.svg" alt="Github Logo" class="w-6 mr-1.5 dark:invert">
+				<span class="text-sm md:text-base">efraimla/quarkus-rest-api</span>
+			</div>
+		</div>
+	</div> -->
 </div>
 
 <style lang='scss'>
