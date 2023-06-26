624f1c7d9f925d5103affac6c18c4ba1f94d0ded
Efraim LA <efraim.lopez@fonyou.com>
2023-06-26 14:12:30 -0600

    init

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<section id='not-found'>
+	<h1 class='text-4xl'>Error!</h1>
+	<h2>Page not found</h2>
+
+	<br>
+	<a href='/'>You can go home clicking <u>here</u></a>
+</section>
+
+<style lang='postcss'>
+    section {
+        @apply min-h-screen overflow-hidden m-0 p-0;
+        @apply bg-gray-600 text-white;
+        @apply flex flex-col items-center justify-center ;
+    }
+
+    @media (prefers-color-scheme: dark) {
+
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<section id='home'>
+	<div>
+
+	</div>
+</section>
+
+<style lang='scss'>
+  section {
+    #home {
+      @apply min-h-screen;
+    }
+  }
+</style>
